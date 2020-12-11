46c1e2b14100ec373283ef77b39b77e50925d4f8
iago <iago@DESKTOP-KQHFD1U>
2020-12-12 00:24:50 +0400

    OnModelCreating

---
+++ PhysicalPersonsDirectory.Domain/Migrations/20201211201832_MyFirstMigration.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhysicalPersonsDirectory.Domain.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Citys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CityName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City_ID", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    GenderName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gender_ID", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    PhoneTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneType_ID", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    RelationTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelationType_ID", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Lname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PersonalNumber = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImagePatch = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GenderId = table.Column<int>(type: "int", nullable: false),
+                    CityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person_ID", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persons_Citys_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Citys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Persons_Genders_GenderId",
+                        column: x => x.GenderId,
+                        principalTable: "Genders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonPhones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhoneTypeId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonPhone_ID", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonPhones_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonPhones_PhoneTypes_PhoneTypeId",
+                        column: x => x.PhoneTypeId,
+                        principalTable: "PhoneTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelatedPersons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    RelatedPersonId = table.Column<int>(type: "int", nullable: false),
+                    RelationTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelatedPerson_ID", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RelatedPersons_Persons_RelatedPersonId",
+                        column: x => x.RelatedPersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RelatedPersons_RelationTypes_RelationTypeId",
+                        column: x => x.RelationTypeId,
+                        principalTable: "RelationTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonPhones_PersonId",
+                table: "PersonPhones",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonPhones_PhoneTypeId",
+                table: "PersonPhones",
+                column: "PhoneTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_CityId",
+                table: "Persons",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_GenderId",
+                table: "Persons",
+                column: "GenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedPersons_RelatedPersonId",
+                table: "RelatedPersons",
+                column: "RelatedPersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedPersons_RelationTypeId",
+                table: "RelatedPersons",
+                column: "RelationTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonPhones");
+
+            migrationBuilder.DropTable(
+                name: "RelatedPersons");
+
+            migrationBuilder.DropTable(
+                name: "PhoneTypes");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "RelationTypes");
+
+            migrationBuilder.DropTable(
+                name: "Citys");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+        }
+    }
+}
