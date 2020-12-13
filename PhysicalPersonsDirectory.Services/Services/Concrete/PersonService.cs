dcff498d64047fc2f73dc44fedf3962feb10a8e1
iago <iago@DESKTOP-KQHFD1U>
2020-12-13 20:46:01 +0400

    PersonService

---
+++ PhysicalPersonsDirectory.Services/Services/Concrete/PersonService.cs
@@ -0,0 +1,17 @@
+﻿using PhysicalPersonsDirectory.Domain;
+using PhysicalPersonsDirectory.Services.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicalPersonsDirectory.Services.Services.Concrete
+{
+    public class PersonService : IPersonService
+    {
+        private readonly PhysicalPersonsContext _db;
+        public PersonService(PhysicalPersonsContext db)
+        {
+            _db = db;
+        }
+    }
+}
