5494afb43da5f75cf17caa11564973bf62eb2fcf
iago <iago@DESKTOP-KQHFD1U>
2020-12-14 15:06:53 +0400

    AddPersonRequest

--- PhysicalPersonsDirectory.Services.Models/Person/Add/AddPersonRequest.cs
+++ PhysicalPersonsDirectory.Services.Models/Person/Add/AddPersonRequest.cs
@@ -1,10 +1,17 @@
 ﻿using PhysicalPersonsDirectory.Services.Models.Person.Shared;
+using System;
 using System.Collections.Generic;
 
 namespace PhysicalPersonsDirectory.Services.Models.Person.Add
 {
-    public class AddPersonRequest : PersonDetailsBaseModel
+    public class AddPersonRequest 
     {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string PersonalNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int GenderId { get; set; }
+        public int CityId { get; set; }
         public List<PersonPhoneBaseModel> PersonPhones { get; set; }
     }
 }
--- PhysicalPersonsDirectory.Services.Models/Person/Edit/EditPersonRequest.cs
+++ PhysicalPersonsDirectory.Services.Models/Person/Edit/EditPersonRequest.cs
@@ -1,11 +1,18 @@
 ﻿using PhysicalPersonsDirectory.Services.Models.Person.Shared;
+using System;
 using System.Collections.Generic;
 
 namespace PhysicalPersonsDirectory.Services.Models.Person.Edit
 {
-    public class EditPersonRequest : PersonDetailsBaseModel
+    public class EditPersonRequest 
     {
         public int? PersonId { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string PersonalNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int GenderId { get; set; }
+        public int CityId { get; set; }
         public List<PersonPhoneModel> PersonPhones { get; set; }
     }
 }
