7f382f89768b1a5947b3ba5fe08c745b7bc9be8e
iago <iago@DESKTOP-KQHFD1U>
2020-12-18 23:18:03 +0400

    .

--- PhysicalPersonsDirectory.Api/Startup.cs
+++ PhysicalPersonsDirectory.Api/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PhysicalPersonsDirectory.Api.CustomExceptionMiddleware;
+
 using PhysicalPersonsDirectory.Api.Extensions;
 using PhysicalPersonsDirectory.Api.Filters;
 using PhysicalPersonsDirectory.Domain;
